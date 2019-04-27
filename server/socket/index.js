dc30b3fc428e11ec8618861ae3b9f38f6586cf0b
Keith Nocera <knocera@gmail.com>
2019-04-27 13:32:37 -0400

    Added server side socket listeners for stages of game, now creating functions within the listeners

--- server/socket/index.js
+++ server/socket/index.js
@@ -21,34 +21,23 @@ module.exports = io => {
     })
 
 
+    //I tried to order these in the order they will be called when a user tries to create a new game
+     //Events: Create gameroom ==> join the game room ==> join a team ==> select a role ==>
+     //        create a new game ==> shuffleBoard? ==> submit clue ==> vote ==> revealCard ==> nextTurn
 
     //Creatign a room requires/data will = : host, roomName, roomCode
-
     socket.on('createRoom', (data) => {
      createRoom(socket, data)})
-
 
     //join room requires the same data as create room
     socket.on('joinRoom', (data) => {
      joinRoom(socket, data)})
 
-    // When client leaves the room we will remove them from the rooms player list
-    socket.on('leaveRoom', () => {
-     leaveRoom(socket)})
 
-     //called when a client closes the browser or loses connection
-    socket.on('disconnect', () => {
-      console.log(`Connection ${socket.id} has left the building`)
-      socketDisconnect(socket)
-    })
-
-    //Things  I need:  Join Team, New Game, endTurn, Submit clue, Vote
-
-    // Join team need team designatio
-    // First make sure the player doesnt already exist
-    // Add player to the player list
-    // set players team value
-    // update the game room to reflect the new player
+    // Join Team
+    // First make sure the player doesnt already exist ==> Add player to the player list-->
+    // set players team value to data update the game room to reflect the new player
+    // Data = players team selection
     socket.on('joinTeam', (data) => {
       if (!PlayerList[socket.id]) {return}
       let  player = PlayerList[socket.id]
@@ -56,6 +45,47 @@ module.exports = io => {
       gameUpdate(player.room)
     })
 
+    //Choose role
+    socket.on('chooseRole', (data) => {chooseRole(socket, data)})
+    //Create a new game
+    socket.on('newGame', ()=> newGame(socket))
+
+    //Spymaster submits a clue, and the clue is shown to all players
+    socket.on('submitClue', (data) => {
+
+    })
+
+    // Clicked/Reveal card
+    //Need to show which card has been selected
+    //Result of clicking a card will depend on player.role. If player.role == Spymaster --> reveal card || guesser --> vote
+    //Data = cardClicked (its index),
+    socket.on('clickedCard', (data) => {clickedCard(socket, data)})
+
+    //Submit Guess
+    //Card with most votes is revealed --> if matches team color --> restart voting phase if revealed card !== team color --> nextTurn()
+    // Data = card index with most votes
+    socket.on('submitGuess', (data) => {submitGuess(socket, data)})
+
+    // End Turn
+    // need seperate end turn functionality as teams may voluntarily end their turn without guessing
+    // need to get the Client's room and swith Room.Game.Turn
+    // update room to reflect change
+    socket.on('endTurn', () => {
+      if (!PlayerList.socket.id) {return}
+      const room = PlayerList[socket.id].room
+      GameRoomList[room].game.nextTurn()
+      gameUpdate(room)
+    })
+
+
+    //Clients need to be able to leave.  When client leaves the room we will remove them from the rooms player list
+    socket.on('leaveRoom', () => {leaveRoom(socket)})
+
+     //called when a client closes the browser or loses connection
+    socket.on('disconnect', () => {
+      console.log(`Connection ${socket.id} has left the building`)
+      socketDisconnect(socket)
+    })
 
   })
 }
